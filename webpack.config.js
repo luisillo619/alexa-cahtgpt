e1a8172fa2961acdd878161a52a89895d903e687
luis.rangel <luis.rangel@adv-ic.com>
2024-12-15 14:41:43 -0600

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 // webpack.config.js
-import path from 'path';
-import nodeExternals from 'webpack-node-externals';
+const path = require('path');
+const nodeExternals = require('webpack-node-externals'); // Instala antes con: npm install webpack-node-externals --save-dev
 
-export default {
+module.exports = {
   entry: './src/index.js',
   target: 'node',
   externals: [nodeExternals()],
   mode: 'production',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(process.cwd(), 'dist')
+    path: path.resolve(__dirname, 'dist')
   }
 };
